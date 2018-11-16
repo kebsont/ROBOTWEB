0549b1343ae5f90a61cdac540a05a77f50643407
KevFleu <kevin.fleurit@gmail.com>
2018-11-16 14:14:28 +0100

    Extraction des liens

--- Bot.java
+++ Bot.java
@@ -1,16 +1,57 @@
 import java.net.*;
 import java.io.*;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 class Bot {
-    public int urlReader(String url) throws Exception { //Affiche le contenu de l'adresse URL (ligne par ligne)
+    public int urlReader(String url) throws Exception { //Affiche les liens de l'URL
         URL oracle = new URL(url);
-        BufferedReader in = new BufferedReader(
-        new InputStreamReader(oracle.openStream()));
-        String inputLine;
-        while ((inputLine = in.readLine()) != null){
-                System.out.println(inputLine);
+        try{ //Lecture et extraction des liens
+            BufferedReader in = new BufferedReader(
+            new InputStreamReader(oracle.openStream()));
+            String inputLine;
+
+            while ((inputLine = in.readLine()) != null){
+                System.out.print(regExp(inputLine));
+            }
+
+            in.close();
+            return 0;
+
+        }catch(Exception e){
+            System.out.println("Impossible d'accéder à l'URL");
+            return 1;
         }
-        in.close();
-        return 0;
+    }
+
+    private String regExp(String line){
+        //Création de la RegEx
+        Pattern p = Pattern.compile("(.*?)<a(.?)href=\"(.+)\" (.*?)>(.+)</a>(.*?)");
+        Matcher m = p.matcher(line);
+
+        //Extraction de la chaine de caractère (liens)
+        if (m.find()) {
+            // get the matching group
+            String codeGroup = m.group(3);
+            codeGroup = codeGroup.split("\"")[0];
+
+            //Tri des lien (uniquement "http... && /...")
+            if(codeGroup.length()>4){
+                String begHttp = codeGroup.substring(0, 4);
+                String begSlash = codeGroup.substring(0, 1);
+                if(begHttp.equals("http")){
+                    // System.out.println("a"+begHttp+"b");
+                    return (codeGroup + "\n");
+                }else if(begSlash.equals("/")){
+                    return (codeGroup + "\n");
+                }else{
+                    return "";
+                }
+            }else{
+                return "";
+            }
+        }else{
+            return "";
+        }
     }
 }
--- Main.java
+++ Main.java
@@ -4,6 +4,6 @@ import java.io.*;
 class Main{
     public static void main(String[] args) throws Exception {
         Bot bot = new Bot();
-        System.out.println(bot.urlReader("https://www.oracle.com"));
+        bot.urlReader("https://www.oracle.com");
     }
 }

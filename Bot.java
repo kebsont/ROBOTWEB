036bc370ee1661c3c91292e17581ac93a44d201c
KevFleu <kevin.fleurit@gmail.com>
2018-11-23 11:46:29 +0100

    Teste des liens (externes) retourne le nombre de liens fonctionnels

--- Bot.java
+++ Bot.java
@@ -2,25 +2,72 @@ import java.net.*;
 import java.io.*;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 class Bot {
     public int urlReader(String url) throws Exception { //Affiche les liens de l'URL
-        URL oracle = new URL(url);
+        URL adr = new URL(url);
+        int res = 0;
+        ArrayList<String> listOfLink = getListLink(adr);
+        for(int i=0; i<listOfLink.size(); i++){ //test des liens
+            String linkOk = checkLink(listOfLink.get(i));
+            System.out.print(i + ": ");
+            if((linkOk != "0") && (linkOk != "1")){
+                res++;
+                System.out.println(linkOk);
+            }else{
+                System.out.println("    x - " + linkOk);
+            }
+        }
+        System.out.println();
+        return (res);
+    }
+
+    private ArrayList<String> getListLink(URL url){
+        ArrayList<String> listOfLink = new ArrayList<String>();
         try{ //Lecture et extraction des liens
             BufferedReader in = new BufferedReader(
-            new InputStreamReader(oracle.openStream()));
+            new InputStreamReader(url.openStream()));
             String inputLine;
 
-            while ((inputLine = in.readLine()) != null){
-                System.out.print(regExp(inputLine));
+            while ((inputLine = in.readLine()) != null){ //Remplissage du tableau avec les liens
+                String resRegExp = regExp(inputLine);
+                if(!resRegExp.equals("")){
+                    listOfLink.add(listOfLink.size(), resRegExp);
+                }
+            }
+
+            for(int i=0; i<listOfLink.size(); i++){ //Affichage de la liste des liens
+                // System.out.print(i + ": ");
+                // System.out.println(listOfLink.get(i));
             }
 
             in.close();
-            return 0;
+            return listOfLink;
 
         }catch(Exception e){
             System.out.println("Impossible d'accéder à l'URL");
-            return 1;
+            return listOfLink;
+        }
+    }
+
+    private String checkLink(String url) throws Exception{
+        URL adr = new URL(url);
+        try{ //Lecture et extraction des liens
+            BufferedReader in = new BufferedReader(
+            new InputStreamReader(adr.openStream()));
+
+            if ((in.readLine()) != null){ //Remplissage du tableau avec les liens
+                in.close();
+                return url;
+            }else{
+                return "0";
+            }
+
+        }catch(Exception e){
+            System.out.println("Impossible d'accéder à l'URL");
+            return "1";
         }
     }
 
@@ -38,12 +85,11 @@ class Bot {
             //Tri des lien (uniquement "http... && /...")
             if(codeGroup.length()>4){
                 String begHttp = codeGroup.substring(0, 4);
-                String begSlash = codeGroup.substring(0, 1);
+                // String begSlash = codeGroup.substring(0, 1);
                 if(begHttp.equals("http")){
-                    // System.out.println("a"+begHttp+"b");
-                    return (codeGroup + "\n");
-                }else if(begSlash.equals("/")){
-                    return (codeGroup + "\n");
+                    return (codeGroup);
+                // }else if(begSlash.equals("/")){
+                //     return (codeGroup);
                 }else{
                     return "";
                 }
--- Main.java
+++ Main.java
@@ -4,6 +4,6 @@ import java.io.*;
 class Main{
     public static void main(String[] args) throws Exception {
         Bot bot = new Bot();
-        bot.urlReader("https://www.oracle.com");
+        System.out.println(bot.urlReader("http://www.topito.com/") + " Liens fonctionnels");
     }
 }

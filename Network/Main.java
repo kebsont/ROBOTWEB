1a6eea14a6b2fc072060bb822ea7014a347a8bdf
kebsont <mktapha@gmail.com>
2019-01-16 17:47:01 +0100

    mm longueur donde que kinou

--- Network/Main.java
+++ Network/Main.java
@@ -2,9 +2,9 @@ import java.net.Socket;
 import java.net.ServerSocket;
 public class Main{
   public static void main(String[] args) throws Exception{
-    String host = "192.168.1.10";
+    String host = "192.168.200.74";
      int port = 1445;
-    Server monServeur = new Server();
+    Server monServeur = new Server(host, port);
     monServeur.lance();
     // //new Thread().start();
 
